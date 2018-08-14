acde0c5818a0aaafc5fcd300e285745815031649
NJI <NJI@DESKTOP-PD0R2ED>
2018-08-14 19:56:01 +0900

    1.회원가입 2.관리자가 공지사항 등록하는 게시판

---
+++ FairyTales/target/classes/member_notice.sql
@@ -0,0 +1,56 @@
+
+/* Drop Tables */
+
+DROP TABLE notice CASCADE CONSTRAINTS;
+DROP TABLE member CASCADE CONSTRAINTS;
+
+
+
+
+/* Create Tables */
+
+CREATE TABLE member
+(
+	nser_name varchar2(20) NOT NULL,
+	user_id varchar2(30) NOT NULL,
+	user_pwd varchar2(30) NOT NULL,
+	user_age number(2,0) NOT NULL,
+	phone_num number(10,0) NOT NULL,
+	address varchar2(200),
+	-- 관리자 or 사용자 선택
+	level number(2,0) NOT NULL,
+	date date DEFAULT sysdate,
+	-- 일어동화, 퍼즐코딩
+	subject varchar2(20),
+	PRIMARY KEY (user_id)
+);
+
+
+CREATE TABLE notice
+(
+	user_id varchar2(30) NOT NULL,
+	board_num number(3,0),
+	title varchar2(100),
+	hitcount number(3,0),
+	date date DEFAULT sysdate,
+	content varchar2(2000)
+);
+
+
+
+/* Create Foreign Keys */
+
+ALTER TABLE notice
+	ADD FOREIGN KEY (user_id)
+	REFERENCES member (user_id)
+;
+
+
+
+/* Comments */
+
+COMMENT ON COLUMN member.level IS '관리자 or 사용자 선택';
+COMMENT ON COLUMN member.subject IS '일어동화, 퍼즐코딩';
+
+
+
